2ad3cd01573684d64498cb3787a6981df9354256
EnviouslyEnvy <evancheung24@gmail.com>
2024-01-14 19:21:40 -0500

    Change navbar font wt. fix content css path

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './css/Content.module.css';
+import styles from './Content.module.css';
 
 const Content = ({ title, hr = false, children }) => {
   return (
